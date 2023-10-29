67ee4344caacc3dd1dc7625aff13c929964f5b3f
Oleg Sampara <lazers03@mail.ru>
2023-10-29 16:39:49 +0300

    feature/4: swipability with undo

--- App/app/src/main/java/com/example/app/presentationlayer/fragments/PlacesListFragment.kt
+++ App/app/src/main/java/com/example/app/presentationlayer/fragments/PlacesListFragment.kt
@@ -7,16 +7,22 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.example.app.R
+import com.example.app.datalayer.models.Place
 import com.example.app.presentationlayer.adapters.PlacesListRecyclerViewAdapter
 import com.example.app.presentationlayer.viewmodels.PlacesListFragmentViewModel
+import com.google.android.material.snackbar.Snackbar
+import okhttp3.internal.notify
 
 class PlacesListFragment : Fragment() {
 
     private val viewModel by viewModels<PlacesListFragmentViewModel>()
 
     private val placesListRecyclerViewAdapter = PlacesListRecyclerViewAdapter()
+
+    private lateinit var deletedLocation: Place
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,15 +34,45 @@ class PlacesListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        view.findViewById<RecyclerView>(R.id.locations_rv).apply {
+        val recyclerView = view.findViewById<RecyclerView>(R.id.locations_rv).apply {
             layoutManager = GridLayoutManager(context, 1)
             adapter = placesListRecyclerViewAdapter
         }
-
         viewModel.placesListRecyclerViewAdapter = placesListRecyclerViewAdapter
-
+        val itemTouchHelper = ItemTouchHelper(onMoveCallback)
+        itemTouchHelper.attachToRecyclerView(recyclerView)
         viewModel.onUpdatePlaces()
     }
+
+    private var onMoveCallback =
+        object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT.or(ItemTouchHelper.RIGHT)) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                TODO("Not yet implemented")
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                when (direction) {
+                    ItemTouchHelper.LEFT -> {
+                        deletedLocation = viewModel.placesList[position]
+                        viewModel.placesList.removeAt(position)
+                        placesListRecyclerViewAdapter.notifyItemRemoved(position)
+
+                        view?.let {
+                            Snackbar.make(it.findViewById<RecyclerView>(R.id.locations_rv), "${deletedLocation.name} is deleted", Snackbar.LENGTH_LONG).setAction("Undo", View.OnClickListener {
+                                viewModel.placesList.add(position,deletedLocation)
+                                placesListRecyclerViewAdapter.notifyItemInserted(position)
+                            }).show()}
+
+                    }
+                }
+            }
+
+        }
 
     companion object {
         @JvmStatic
--- App/app/src/main/java/com/example/app/presentationlayer/viewmodels/PlacesListFragmentViewModel.kt
+++ App/app/src/main/java/com/example/app/presentationlayer/viewmodels/PlacesListFragmentViewModel.kt
@@ -3,6 +3,7 @@ package com.example.app.presentationlayer.viewmodels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.app.businesslayer.providers.MapProvider
+import com.example.app.datalayer.models.Place
 import com.example.app.presentationlayer.adapters.PlacesListRecyclerViewAdapter
 import kotlinx.coroutines.launch
 
@@ -12,9 +13,11 @@ internal class PlacesListFragmentViewModel : ViewModel() {
 
     lateinit var placesListRecyclerViewAdapter: PlacesListRecyclerViewAdapter
 
+    lateinit var placesList:MutableList<Place>
+
     fun onUpdatePlaces() {
         viewModelScope.launch {
-            val placesList = mapProvider.getPlaces()
+            placesList = mapProvider.getPlaces().toMutableList()
             placesListRecyclerViewAdapter.submitList(placesList)
         }
     }
