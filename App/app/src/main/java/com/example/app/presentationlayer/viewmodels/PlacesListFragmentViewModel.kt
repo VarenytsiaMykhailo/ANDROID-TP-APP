0f53beb2921e0c4472e83e0fa58fda461b916049
Oleg Sampara <lazers03@mail.ru>
2023-11-08 17:56:55 +0300

    feature/45 reaction post on swipe

--- App/app/src/main/java/com/example/app/presentationlayer/viewmodels/PlacesListFragmentViewModel.kt
+++ App/app/src/main/java/com/example/app/presentationlayer/viewmodels/PlacesListFragmentViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.app.domain.providers.MapProvider
 import com.example.app.datalayer.models.NearbyPlace
+import com.example.app.datalayer.models.PlaceReaction
 import com.example.app.presentationlayer.adapters.PlacesListRecyclerViewAdapter
 import com.example.app.presentationlayer.fragments.placeslistscreen.PlacesListFragment
 import com.google.android.gms.maps.model.LatLng
@@ -64,4 +65,7 @@ internal class PlacesListFragmentViewModel : ViewModel() {
 
     fun onRestorePlace(position: Int, restoredLocation: NearbyPlace) =
         mapProvider.placesList.add(position, restoredLocation)
+
+    suspend fun postSuggestReaction(placeId: String, reaction: PlaceReaction.Reaction) =
+        mapProvider.postSuggestReaction(placeId, reaction)
 }
